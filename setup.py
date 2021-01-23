0940be68f18268c97ab497e5c12d5a5d873a169c
salilathalye <823500+salilathalye@users.noreply.github.com>
2021-01-23 09:54:04 -0500

    feat: update template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='DPhi Data Sprint 21: Classification of Malware with PE headers',
+    author='Salil Athalye',
+    license='MIT',
+)
